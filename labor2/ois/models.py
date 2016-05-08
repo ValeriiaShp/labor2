c5596f71e694147de961a4b36f518e0ac88954b6
Valeriia Shpychka <valeriia.shpychka@ignite.ee>
2016-05-08 18:26:30 +0300

    db structure

--- labor2/ois/models.py
+++ labor2/ois/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Semester(models.Model):
+    number = models.CharField(max_length=11)
+	start_date = models.DateField()
+	end_date = models.DateField()
+	
+class Subject(models.Model):
+	code = models.IntegerField()
+	name =  models.CharField(max_length=400)
+	description =  models.TextField()
+	
+class HomeWork(models.Model):
+	name =  models.CharField(max_length=400)
+	description =  models.TextField()
+
+class HomeWorkUser(models.Model):
+	id_user = models.ForeignKey(User)
+	id_homework = models.ForeignKey(HomeWork)
+	answer = models.TextField()
+	mark = models.IntegerField()
+	
+class SubjectUser(models.Model):
+	id_user = models.ForeignKey(User)
+	id_subject = models.ForeignKey(Subject)
