c65b7ed798e6db615c470536d9e8f6acf99e7299
Valeriia Shpychka <valeriia.shpychka@ignite.ee>
2016-05-10 16:23:26 +0300

    urls

--- labor2/ois/views.py
+++ labor2/ois/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from ois.models import HomeWorkUser
+from ois.models import Notification
 
-# Create your views here.
+
+def notifications(request):
+    notifications = Notification.objects.all()
+    results = [n.as_json() for n in notifications()]
+    return HttpResponse(json.dumps(results), content_type="application/json")
+
+
+def marks(request):
+    marks = HomeWorkUser.objects.all()
+    results = [m.as_json() for m in marks()]
+    return HttpResponse(json.dumps(results), content_type="application/json")
+
+
+def statistics(request):
+    pass
+    # stastistics = Cat.objects.all()
+    # results = [c.as_json() for c in allCats]
+    # return HttpResponse(json.dumps(results), content_type="application/json")
+
+
+def search(request):
+    pass
+    # allCats = Cat.objects.all()
+    # results = [c.as_json() for c in allCats]
+    # return HttpResponse(json.dumps(results), content_type="application/json")
